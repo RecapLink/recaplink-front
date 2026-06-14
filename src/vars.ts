71ea6768bf9db6b148b6c89f794f4e2c517ec368
Malek BOUBAKRI <malek@tuskens.com>
2026-06-14 04:07:51 +0100

    Fix build issue by faking the src folder content

---
+++ src/vars.ts
@@ -0,0 +1,4 @@
+// not used in static files, added to avoid the "... `tsconfig.json` includes `["src"]` and no `src/` directory ..." exists"
+const FB_API = 'https://api.recaplink.tn/api';
+
+console.debug(FB_API);
